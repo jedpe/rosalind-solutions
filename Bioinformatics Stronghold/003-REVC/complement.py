ded8469b8a459381b7274e3f1061e8d6171612aa
jorge <jorge_eduardo2894@hotmail.com>
2023-03-24 22:02:14 -0700

    Formatting

--- Bioinformatics Stronghold/003-REVC/complement.py
+++ Bioinformatics Stronghold/003-REVC/complement.py
@@ -7,14 +7,10 @@ URL: http://rosalind.info/problems/revc/
 
 
 def reverse_complement(s):
-
     # Dictionary of of the base complements
-    base_complement = {"A": "T",
-                       "T": "A",
-                       "C": "G",
-                       "G": "C"}
+    base_complement = {"A": "T", "T": "A", "C": "G", "G": "C"}
 
-    # Since we want the reverse of s we need a way to access it from back to front
+    # We want the reverse of s. We need a way to access it from back to front
     idx_range = range(1, len(s) + 1)
 
     # Get the complement of s
@@ -25,10 +21,8 @@ def reverse_complement(s):
     return s_c
 
 
-if __name__ == '__main__':
-
-    with open('rosalind_revc.txt', 'r') as input_data:
-
+if __name__ == "__main__":
+    with open("rosalind_revc.txt", "r") as input_data:
         # Read the first line of the file
         s = input_data.readline().strip()
 
